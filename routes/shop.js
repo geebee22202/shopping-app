dd2c6446035526ca5a5da4003e5a2685bae6bfa7
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-06-22 23:54:11 -0400

    added css styling to shop page, fixed bug preventing shop.html from being properly served

--- routes/shop.js
+++ routes/shop.js
@@ -5,13 +5,13 @@ const router = express.Router()
 
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>hello from express</h1>"); //default response header is html
+  // res.send("<h1>hello from express</h1>"); //default response header is html
   //below doesn't work becuase it's pointint to root folder
   // res.sendFile('/views/shop.html')
   //dirname is a global variable that holds aboslulte path to project folder
   //this way will work on both linux and windows systems
   //../ means go up one level
-  res.sendFile(path.join(__dirname, '.../', 'views', 'shop.html'));
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router
