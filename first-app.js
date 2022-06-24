24080f90800729844d0825e724752343410c7a13
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-06-23 21:33:00 -0400

    update html/css files to leverage what was being used by instructor

--- first-app.js
+++
@@ -1,7 +0,0 @@
-//in node js, you don't just run the code, but you can also create a serverw with it
-//handle business logis, connect to database, handle request
-//responses = return html, json, files, etc to clients
-
-const fs = require('fs') //file system, one of node's core modules
-
-fs.writeFileSync('hello.txt', 'hello from Node.js')
