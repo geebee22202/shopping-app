9dec176ce3ca1d1c457afa430890a6fd1e3a09f9
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-06-27 21:08:42 -0400

    pug template version

--- app.js
+++ app.js
@@ -57,9 +57,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
+  res.status(404).render('404', {pageTitle: "Page not found"})
 });
 
 // const server = http.createServer(app); removed due to express
--- routes/admin.js
+++ routes/admin.js
@@ -5,21 +5,22 @@ const express = require("express");
 
 const router = express.Router();
 
-const products = []
+const products = [];
 
+//admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', "views", "add-product.html"));
-
+  res.render("add-product", { pageTitle: "Add Product", path: '/admin/add-product' });
   // res.send(
   //   '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   // ); //default response header is html
 });
 //you have use either next or send a response
 
+//admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  products.push({title: req.body.title})
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
 exports.routes = router;
-exports.products = products
+exports.products = products;
